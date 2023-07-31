f2788e891d8b06c3343cc5755a765c4ba0f5970d
Abhie-Tripathi <abhiiii.tripathi@gmail.com>
2023-07-31 19:30:29 +0530

    Sidebar

---
+++ src/components/HomeSection.svelte
@@ -0,0 +1,12 @@
+<!-- src/components/HomeSection.svelte -->
+<script>
+    // You can add any logic specific to HomeSection if needed
+  </script>
+  
+  <div class="home-section">
+    <div class="text">This in Home Section</div>
+  </div>
+  
+  <style>
+    /* Your HomeSection styles ... */
+  </style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,46 @@
+<div class="p-4 sm:ml-64">
+	<div class="grid grid-cols-3 gap-4 mb-4">
+		<div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+      <h1 class="text-white text-3xl">Dashboard</h1>
+    </div>
+		
+    <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+
+    </div>
+
+		<div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+
+    </div>
+
+	</div>
+
+
+
+	<div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+
+  </div>
+
+
+	<div class="grid grid-cols-2 gap-4 mb-4">
+		<div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+
+    </div>
+
+		<div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+
+    </div>
+    
+		<div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+
+    </div>
+
+		<div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+
+    </div>
+	</div>
+
+
+	<div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+
+  </div>
+</div>
